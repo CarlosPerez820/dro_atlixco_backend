1fa003548d3ff73f63589ffea3c10b0b42d549bd
Gideon <perez.lon820@gmail.com>
2024-03-20 20:50:33 -0600

    se esta agregando el servicio de envio de correo y la actualizacion del public

--- routes/send-email.js
+++ routes/send-email.js
@@ -1,28 +1,47 @@
 const nodemailer = require('nodemailer');
 const {Router} = require('express');
-
+const multer = require('multer');
+const router = Router();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports
-    auth: {
-    user: "drop.atlixco@gmail.com",
-    pass: "Cl@v3820.",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+  user: "drop.atlixco@gmail.com",
+  pass: "lsjrhjectkvhuayf",
+},
+});
+
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const router = Router();
+const upload = multer({ storage });
 
-router.post('/send-email', (req, res) => {
-  const { nombre, email, archivos } = req.body;
+router.post('/send-email', upload.array('archivos'), (req, res) => {
+  const { nombre, email, tramite, destinatario} = req.body;
+  const attachments = [];
+
+  // Manejar archivos adjuntos si se incluyen
+  if (req.files && req.files.length > 0) {
+    req.files.forEach(file => {
+      attachments.push({
+        filename: file.filename,
+        path: file.path,
+      });
+    });
+  }
 
   const mailOptions = {
-    from: 'dro.atlixco@gmail.com',
-    to: 'perez.lon820@gmail.com',
-    subject: 'Formulario de contacto',
-    text: `Nombre: ${nombre}\nEmail: ${email}`,
-    
+    from: 'Mensaje enviado por <drop.atlixco@gmail.com>',
+    to: destinatario,
+    subject: 'Requisitos para el tramite: '+tramite,
+    text: `Nombre del solicitante: ${nombre}\nMetodo de contacto: ${email}`,
+    attachments,
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
